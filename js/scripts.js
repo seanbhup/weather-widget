4ec51a564467f20f2500da49b657ede3ac5293fc
Sean Bhupathi <seanbhupathi@gmail.com>
2016-12-24 22:00:01 -0500

    update css, add js

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,19 @@ $(document).ready(function(){
 		// console.log(weatherUrl);
 		$.getJSON(weatherUrl, function(weatherData){
 		console.log(weatherData);
+		var tempMax = weatherData.main.temp_max;
+		var tempMin = weatherData.main.temp_min;
 		var currTemp = weatherData.main.temp;
+		var currHumid = weatherData.main.humidity;
 		var name = weatherData.name;
 		var icon = weatherData.weather[0].icon;
 		var iconUrl = "http://openweathermap.org/img/w/"+icon+".png";
-		$("#current-temp").html("<img src=" +iconUrl+ "> The temperature in " + name + " is currently " + currTemp + "&deg;" + " Fahrenheit");
+		$("#current-temp").html(currTemp + "&deg;F");
+		$("#current-humidity").html(currHumid + "% Humidity");
+		$("#location").html(name);
+		$("#temp-max").html(tempMax);
+		$("#temp-min").html(tempMin);
+		// $("#current-temp").html("<img src=" +iconUrl+ "> The temperature in " + name + " is currently " + currTemp + "&deg;" + " Fahrenheit");
 		// $.getJSON(carbonUrl, function(carbonData){
 		// console.log(carbonData);
 		// var dateTime = weatherData.dt;
@@ -25,35 +33,35 @@ $(document).ready(function(){
 		// var lon = weatherData.coord.lon;
 		// });
 		// $("#carbon-monox").html("hi")
-		var canvas = $("#weather-canvas");
-		var context = canvas[0].getContext("2d");
-		// Set up the outer circle
-		var currentPercent = 0;
-		function animate(current){
-			// Draw the inner circle
-			context.fillStyle = "#706563";
-			context.beginPath();
-			context.arc(200, 75, 65, Math.PI * 0, Math.PI * 2);
-			context.closePath();
-			context.fill();
+		// var canvas = $("#weather-canvas");
+		// var context = canvas[0].getContext("2d");
+		// // Set up the outer circle
+		// var currentPercent = 0;
+		// function animate(current){
+		// 	// Draw the inner circle
+		// 	context.fillStyle = "#706563";
+		// 	context.beginPath();
+		// 	context.arc(200, 75, 65, Math.PI * 0, Math.PI * 2);
+		// 	context.closePath();
+		// 	context.fill();
 
-			// Draw the outer line
-			context.lineWidth = 7; //make a thick outer line
-			context.strokeStyle = "#9DC7C8";
-			context.beginPath();
-			context.arc(200, 75, 69, (Math.PI * 1.5), (Math.PI * 2 * current) + (Math.PI * 1.5));
-			context.stroke();
-			currentPercent++; //increment currPercent
-			if(currentPercent < currTemp){
-				requestAnimationFrame(function(){
-					animate(currentPercent / 100);
-				})
-			}
-			// context.closePath();
-			// context.fill();
+		// 	// Draw the outer line
+		// 	context.lineWidth = 7; //make a thick outer line
+		// 	context.strokeStyle = "#9DC7C8";
+		// 	context.beginPath();
+		// 	context.arc(200, 75, 69, (Math.PI * 1.5), (Math.PI * 2 * current) + (Math.PI * 1.5));
+		// 	context.stroke();
+		// 	currentPercent++; //increment currPercent
+		// 	if(currentPercent < currTemp){
+		// 		requestAnimationFrame(function(){
+		// 			animate(currentPercent / 100);
+		// 		})
+		// 	}
+		// 	// context.closePath();
+		// 	// context.fill();
 
-		}
-		animate();
+		// }
+		// animate();
 	});
 
 	
